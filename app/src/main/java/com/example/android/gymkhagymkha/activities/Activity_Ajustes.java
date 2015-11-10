3ef83730e198e2ba5a5fadbb9bc73a6e76725f3d
Fran <gymkhagymkha@gmail.com>
2015-11-11 00:31:36 +0100

    Añadidos para cambiar de tema

--- app/src/main/java/com/example/android/gymkhagymkha/activities/Activity_Ajustes.java
+++ app/src/main/java/com/example/android/gymkhagymkha/activities/Activity_Ajustes.java
@@ -2,6 +2,9 @@ package com.example.android.gymkhagymkha.activities;
 
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,13 +12,20 @@ import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.android.gymkhagymkha.R;
 
 public class Activity_Ajustes extends AppCompatActivity {
 
     Toolbar toolbar;
-    String inservible = "";
+    Button btnCambiarTema;
+    ImageButton ibPurple, ibRed, ibBlue, ibGreen, ibOrange;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +43,48 @@ public class Activity_Ajustes extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(colorPrimaryDark);
         }
+
+        btnCambiarTema = (Button) this.findViewById(R.id.btnCambiarTema);
+        btnCambiarTema.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(Activity_Ajustes.this);
+                dialog.setTitle(R.string.title_cambiarTema);
+                dialog.setContentView(R.layout.dialog_cambiar_tema);
+                dialog.show();
+            }
+        });
+/*
+        ibPurple = (ImageButton) this.findViewById(R.id.ibPurple);
+        ibRed = (ImageButton) this.findViewById(R.id.ibRed);
+        ibBlue = (ImageButton) this.findViewById(R.id.ibBlue);
+        ibGreen = (ImageButton) this.findViewById(R.id.ibGreen);
+        ibOrange = (ImageButton) this.findViewById(R.id.ibOrange);
+        ibPurple.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(Activity_Ajustes.this, "Morado", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ibRed.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(Activity_Ajustes.this, "Rojo", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ibBlue.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(Activity_Ajustes.this, "Azul", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ibGreen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(Activity_Ajustes.this, "Verde", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ibOrange.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(Activity_Ajustes.this, "Naranja", Toast.LENGTH_SHORT).show();
+            }
+        });
+*/
     }
 
     @Override
